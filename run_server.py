66e68255db54f2ca4f7f974861b73e1ab0f95715
fiascototal <fiascototal@wouaf.org>
2019-08-19 23:54:32 +0200

    add a root script to run the server

---
+++ run_server.py
@@ -0,0 +1,15 @@
+import argparse
+from fake_ssh.server import FakeSshServer
+
+
+def _main():
+    parser = argparse.ArgumentParser(description="Run the fake ssh server")
+    parser.add_argument("-f", "--config", help="You can specify a custom configuration file", default="")
+    args = parser.parse_args()
+
+    srv = FakeSshServer(config_filename=args.config)
+    srv.start()
+
+
+if __name__ == "__main__":
+    _main()
