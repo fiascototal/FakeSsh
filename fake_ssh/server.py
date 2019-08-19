3128648822dbc0a3187f179605b6d559c68df4fd
fiascototal <fiascototal@wouaf.org>
2019-08-19 23:13:59 +0200

    check if the user is banned

--- fake_ssh/server.py
+++ fake_ssh/server.py
@@ -1,5 +1,7 @@
 import socket
+import datetime
 from fake_ssh import config
+from fake_ssh.database import connect, create_tables, DbIp, DbUsername, DbPassword, DbBanned, DbValidAccount, DbLog
 
 
 class FakeSshServer(object):
@@ -7,8 +9,10 @@ class FakeSshServer(object):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._sock.bind((config.NETWORK_INTERFACE, config.NETWORK_TCP_PORT))
+        connect()
+        create_tables()
 
-    def run(self):
+    def start(self):
         self._sock.listen(100)
         print("Listening for connection ...")
 
@@ -16,3 +20,17 @@ class FakeSshServer(object):
             client, addr = self._sock.accept()
             (client_ip, client_port) = addr
             print("Got a connection from %s:%d" % (client_ip, client_port))
+
+            if FakeSshServer.is_ban(client_ip):
+                print("This client is still ban")
+                continue
+
+    @staticmethod
+    def is_ban(ip_addr):
+        if DbIp.select().where(DbIp.value == ip_addr).count() == 0:
+            return False
+        ip_obj = DbIp.get(value=ip_addr)
+        for ban in DbBanned.select().where(DbBanned.ip == ip_obj):
+            if ban.date + datetime.timedelta(days=ban.duration) > datetime.datetime.now():
+                return True
+        return False
