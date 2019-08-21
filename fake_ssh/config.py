62f0b2615b370c0b88d522f2e18a1e58c3768d9b
fiascototal <fiascototal@wouaf.org>
2019-08-21 21:47:16 +0200

    add a setting to manage the period of check before being banned

--- fake_ssh/config.py
+++ fake_ssh/config.py
@@ -15,6 +15,7 @@ period_size=INTEGER
 
 [ban]
 limit=INTEGER
+limit_period=INTEGER
 nb_day=INTEGER
 
 [network]
@@ -31,6 +32,7 @@ SQLITE_PATH = "logs.db"
 EMAIL_ADDR = None
 EMAIL_PERIOD = 7
 BAN_LIMIT = 50
+BAN_LIMIT_PERIOD = 7
 BAN_DAY_MAX = 30
 NETWORK_TCP_PORT = 22
 NETWORK_INTERFACE = ""
@@ -39,7 +41,10 @@ CRYPTO_KEY_SIZE = 1024
 
 
 def load_config(filename=""):
-    global SLOW_MILLISEC, SQLITE_PATH, EMAIL_ADDR, EMAIL_PERIOD, BAN_LIMIT, BAN_DAY_MAX, NETWORK_TCP_PORT
+    global SLOW_MILLISEC, SQLITE_PATH
+    global EMAIL_ADDR, EMAIL_PERIOD
+    global BAN_LIMIT, BAN_DAY_MAX, BAN_LIMIT_PERIOD
+    global NETWORK_TCP_PORT, NETWORK_INTERFACE
     global CRYPTO_KEY_SIZE, CRYPTO_KEY_TYPE
 
     config = configparser.ConfigParser()
@@ -55,19 +60,27 @@ def load_config(filename=""):
             SLOW_MILLISEC = int(config["general"]["slow_millisec"])
         if "sqlite_path" in config["general"]:
             SQLITE_PATH = config["general"]["sqlite_path"]
+
     if "mail" in config:
         if "address" in config["mail"]:
             EMAIL_ADDR = config["mail"]["address"]
         if "period" in config["mail"]:
             EMAIL_PERIOD = int(config["mail"]["period"])
+
     if "ban" in config:
         if "limit" in config["ban"]:
             BAN_LIMIT = int(config["ban"]["limit"])
+        if "limit_period" in config["ban"]:
+            BAN_LIMIT_PERIOD = int(config["ban"]["limit_period"])
         if "nb_day" in config["ban"]:
             BAN_DAY_MAX = int(config["ban"]["nb_day"])
+
     if "network" in config:
         if "listen_port" in config["network"]:
             NETWORK_TCP_PORT = int(config["network"]["listen_port"])
+        if "interface" in config["network"]:
+            NETWORK_INTERFACE = config["network"]["interface"]
+
     if "crypto" in config:
         if "key_type" in config["crypto"]:
             CRYPTO_KEY_TYPE = config["crypto"]["key_type"]
