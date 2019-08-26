a72f949b95f348edcd48e1e0374fb51db425da13
fiascototal <fiascototal@wouaf.org>
2019-08-26 23:22:09 +0200

    implement a tiny scheduler to call periodically a mailer

---
+++ fake_ssh/schedule.py
@@ -0,0 +1,36 @@
+"""
+basic implementation of a timer that schedule threaded actions every X days
+"""
+
+
+import datetime
+import threading
+
+# global that contains the list of scheduled callbacks
+_waiting_list = list()
+
+
+def do_every(callback, nb_sec_period=0, nb_days_period=0):
+    """ add a new callback and a period. It will run the callback every period """
+
+    new_item = {
+        "callback": callback,
+        "period": datetime.timedelta(seconds=nb_sec_period, days=nb_days_period),
+        "time_start": datetime.datetime.now(),
+    }
+    _waiting_list.append(new_item)
+
+
+def tick():
+    """ main function: check if we need to call the waiting list """
+
+    now = datetime.datetime.now()
+    for item in _waiting_list:
+        if item["time_start"] + item["period"] < now:
+
+            # call the callback
+            t = threading.Thread(target=item["callback"])
+            t.start()
+
+            # reset the counter
+            item["time_start"] = now
--- run_server.py
+++ run_server.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import time
 from fake_ssh.server import FakeSshServer
 from fake_ssh import config
 from fake_ssh import database
+from fake_ssh import schedule
+from fake_ssh.mailer import send_mail
 
 
 def _main():
@@ -14,8 +17,14 @@ def _main():
     config.load_config(args.config)
     database.init_db()
 
+    schedule.do_every(send_mail, nb_days_period=config.EMAIL_PERIOD)
+
     srv = FakeSshServer()
     srv.start()
+
+    while True:
+        schedule.tick()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
